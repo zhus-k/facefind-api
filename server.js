563e6727449677994112d456ca5be6638ecc2dff
zhus-k <76853151+zhus-k@users.noreply.github.com>
2021-08-11 18:05:36 -0400

    updated database connection

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const app = express();
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString : process.env.DATABASE_URL,
-        ssl: true
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
